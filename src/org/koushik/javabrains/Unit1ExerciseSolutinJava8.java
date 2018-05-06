72e50019a40ac23b41efec708eb81842b123aacc
alamnr <alamnr@gmail.com>
2018-05-06 13:40:11 +0600

    Commit for Lambda Basic 15 Using Function interface

--- src/org/koushik/javabrains/Unit1ExerciseSolutinJava8.java
+++ src/org/koushik/javabrains/Unit1ExerciseSolutinJava8.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Unit1ExerciseSolutinJava8 {
 public static void main(String[] args) {
@@ -26,46 +28,53 @@ public static void main(String[] args) {
 		// Step-2: Create a method that prints all element in list
 		System.out.println("Print all person");
 		//printAll(people);
-		printConditionally(people, p->true);
+		//printConditionally(people, p->true);
+		printConditionally(people, p->true,p->System.out.println(p));
 		
 		
 		// Step-3: Create a method that prints all the people that have last name begining with C
 		
 		System.out.println("Print person last name begining with C");
 		//printLastNameBeginingWithC(people);
-		printConditionally(people, p->p.getLastName().startsWith("C"));
+		//printConditionally(people, p->p.getLastName().startsWith("C"));
+		printConditionally(people, p->p.getLastName().startsWith("C"),p->System.out.println(p));
 		
 		
 		System.out.println("Print person first name begining with B");
-		printConditionally(people, p->p.getFirstName().startsWith("B"));
+		//printConditionally(people, p->p.getFirstName().startsWith("B"));
+		printConditionally(people, p->p.getFirstName().startsWith("B"),p->System.out.println(p));
 	}
 
-	private static void printLastNameBeginingWithC(List<Person> people) {
-	 
-		 for (Person p: people){
-			 if(p.getLastName().startsWith("C"))
-			 {
-				System.out.println(p);
-			 }
-			}
-	
-}
-
-	private static void printAll(List<Person> people){
-		for (Person p: people){
-			System.out.println(p);
-		}
-	}
-	
-	private static void printConditionally(List<Person> people, Condition condition){
+		
+	/*private static void printConditionally(List<Person> people, Condition condition){
 		 for (Person p: people){
 			 if(condition.test(p))
 			 {
 				System.out.println(p);
 			 }
 			}
-	}
+	}*/
 	
+	/*private static void printConditionally(List<Person> people, Predicate<Person> predicate){
+		 for (Person p: people){
+			 if(predicate.test(p))
+			 {
+				System.out.println(p);
+			 }
+			}
+	}
+*/
+	
+	private static void printConditionally(List<Person> people, Predicate<Person> predicate,Consumer<Person> consumer)
+	{
+		 for (Person p: people){
+			 if(predicate.test(p))
+			 {
+				consumer.accept(p);
+			 }
+			}
+	}
+
 }
 
 
