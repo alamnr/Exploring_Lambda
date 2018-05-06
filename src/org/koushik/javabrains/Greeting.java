ffa017b33b1f89e8a886ac301ebf68ee91587344
alamnr <alamnr@gmail.com>
2018-05-06 12:04:31 +0600

    Commit for Lambda Basic 12-Functional Interface

--- src/org/koushik/javabrains/Greeting.java
+++ src/org/koushik/javabrains/Greeting.java
@@ -1,5 +1,7 @@
 package org.koushik.javabrains;
 
+// Recommended to use this annotation for Lamda Functional Interface, so that other can't add new abstract method in this interface 
+@FunctionalInterface
 public interface Greeting {
 	
 	public void perform();
